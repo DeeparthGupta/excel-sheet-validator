b4339c0c7f85fdb016358ffcff7678b91f54ff2e
Deeparth <dprthgpta@gmail.com>
2025-08-28 01:21:36 +0530

    Corrected output file naming and indentation,

--- backend/src/fileInjestor.ts
+++ backend/src/fileInjestor.ts
@@ -5,63 +5,63 @@ import path from "path";
 
 
 export async function convertExcelToJson(
-  inputPath: string,
-  outputDir: string,
-  deleteSource:boolean = false
+    inputPath: string,
+    outputDir: string,
+    deleteSource: boolean = false
 ): Promise<string> {
-  const fileName = `${Date.now()}.json`;
-  const outputPath = path.join(outputDir, fileName);
+    const fileName = path.basename(inputPath, path.extname(inputPath)) + "_injested" + ".json";;
+    const outputPath = path.join(outputDir, fileName);
 
-  const stream = await getXlsxStream({
-    filePath: inputPath,
-    sheet: 0,
-    withHeader: true
-  });
-
-  return new Promise((resolve, reject) => {
-    const ws = createWriteStream(outputPath, { encoding: "utf-8" });
-    let headers: string[] = [];
-      let isFirst = true;
-      let rowIndex = 0
-
-    ws.write("["); // start JSON array
-
-    stream.on("data", ({ formatted, header }) => {
-        if (headers.length === 0) {
-            if (header && header.length > 0) {
-                headers = header;
-            }
-            else if (formatted.obj) {
-                headers = Object.keys(formatted.obj)
-            }
-        }
-
-        if (!headers.length) {
-            return;
-        }
-
-        const rowObj: Record<string, any> = headers.reduce((acc, key) => {
-            acc[key] = formatted.obj[key] ?? null;
-            return acc;
-        }, {});
-
-        rowObj.index = rowIndex++;
-
-      ws.write((isFirst ? "" : ",") + JSON.stringify(rowObj));
-      isFirst = false;
+    const stream = await getXlsxStream({
+        filePath: inputPath,
+        sheet: 0,
+        withHeader: true
     });
 
-    stream.on("end", async () => {
-      ws.end("]"); // end JSON array
-      try {
-        if(deleteSource) await fs.unlink(inputPath); // delete source file
-        resolve(outputPath); //Return path to json file
-      } catch (err) {
-        reject(err);
-      }
-    });
+    return new Promise((resolve, reject) => {
+        const ws = createWriteStream(outputPath, { encoding: "utf-8" });
+        let headers: string[] = [];
+        let isFirst = true;
+        let rowIndex = 0
 
-    stream.on("error", (err) => reject(err));
-    ws.on("error", (err) => reject(err));
-  });
+        ws.write("["); // start JSON array
+
+        stream.on("data", ({ formatted, header }) => {
+            if (headers.length === 0) {
+                if (header && header.length > 0) {
+                    headers = header;
+                }
+                else if (formatted.obj) {
+                    headers = Object.keys(formatted.obj)
+                }
+            }
+
+            if (!headers.length) {
+                return;
+            }
+
+            const rowObj: Record<string, any> = headers.reduce((acc, key) => {
+                acc[key] = formatted.obj[key] ?? null;
+                return acc;
+            }, {});
+
+            rowObj.index = rowIndex++;
+
+            ws.write((isFirst ? "" : ",") + JSON.stringify(rowObj));
+            isFirst = false;
+        });
+
+        stream.on("end", async () => {
+            ws.end("]"); // end JSON array
+            try {
+                if (deleteSource) await fs.unlink(inputPath); // delete source file
+                resolve(outputPath); //Return path to json file
+            } catch (err) {
+                reject(err);
+            }
+        });
+
+        stream.on("error", (err) => reject(err));
+        ws.on("error", (err) => reject(err));
+    });
 }
