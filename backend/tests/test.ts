325c41a60f146af98987512d887e72c979071310
Deeparth <dprthgpta@gmail.com>
2025-08-28 13:31:02 +0530

    Made testing easier by creating a testing config.

--- backend/tests/test.ts
+++ backend/tests/test.ts
@@ -1,9 +1,14 @@
 import path from "path";
-import { convertExcelToJson } from "./fileInjestor.js";
+import { convertExcelToJson } from "./../src/fileInjestor.js";
 
 async function main() {
-    // Replace with the path to your Excel file
-    const excelPath = path.resolve("C:/Repos/customer-validator-frappe/backend/uploads/test_sheet.xlsx");
+    const fileName = process.argv[2];
+    if (!fileName) {
+        console.error("Specify filename");
+        process.exit(1);
+    }
+
+    const excelPath = path.resolve("C:/Repos/customer-validator-frappe/backend/uploads/", fileName);
     const outputDir = path.resolve("C:/Repos/customer-validator-frappe/backend/uploads/");
 
     try {
