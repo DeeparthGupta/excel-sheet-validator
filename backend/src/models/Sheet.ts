65d1029596515f7055ce2565e11d3ab32c7983b4
Deeparth <dprthgpta@gmail.com>
2025-10-01 16:12:57 +0530

    Refactored Sheet class. Added row and column management.

--- backend/src/models/Sheet.ts
+++ backend/src/models/Sheet.ts
@@ -1,28 +1,62 @@
+type RowID = bigint;
+type ColumnID = string;
 type RowData = Record<string, any>;
-type UniquenessMap = Map<string, Map<any, Set<number>>>
+type UniquenessMap = Map<string, Map<any, Set<number>>>;
+type ErrorMap = Map<RowID, string[]>;
 
-interface rowMetaData {
-    id: number;
-    valid: boolean;
-    errors: string;
+type ColumnData = {
+    displayName: string;
+    type?: any;
 }
 
+interface RowReference{
+    sheetId: string;
+    rowID: number;
+}
+
+interface SerializedRow {
+    [key: string]: any;
+    id: number;
+    valid: boolean;
+    errors: string[];
+}
+
+
 export class Sheet{
-    name: string;
-    rows: Record<string, any>[];
-    columnConstraints?: Map<string, any>;
+    sheetID: string;
+    sheetName: string;
+    columns: Map<ColumnID, ColumnData> = new Map();
+    rows: Map<RowID, RowData> = new Map();
+    graveyard: RowID[] = []; 
+    columnConstraints?: Map<ColumnID, any>;
+    errors?: ErrorMap;
+
     private _validRowCount: number = 0;
     private _invalidRowCount: number = 0;
-    private _uniquenessMap: UniquenessMap = new Map(); 
+    private _uniquenessMap: UniquenessMap = new Map();
+    private _errorMap: ErrorMap = new Map();
+    private _columnCounter: number = 1;
+    private _rowCounter: RowID = 1n;
 
-    constructor(name: string, rows: Record<string, any>[], columnConstraints: Map<string, any>) {
-        this.name = name;
-        this.rows = rows;
-        this.columnConstraints = columnConstraints;
+    constructor(id: string, name: string, columns?: ColumnData[] | string[], rows?: RowData[]) {
+        this.sheetID = id;
+        this.sheetName = name;
+
+        if (columns && columns.length > 0) {
+            columns.forEach((column: ColumnData | string) => {
+                this.addColumn(column);
+            });
+        }
+
+        if (rows && rows.length > 0) {
+            rows.forEach(row => {
+                this.addRow(row);
+            })
+        }
     }
 
     get rowCount() {
-        return this.rows.length;
+        return this.rows.size;
     }
     
     get validRowCount() {
@@ -33,6 +67,44 @@ export class Sheet{
         return this._invalidRowCount;
     }
 
+    addColumn(column: ColumnData | string): void {
+        const columnId:string = `col${this._columnCounter++}`;
+        if (typeof column === 'string'){
+            const columnObj: ColumnData = {
+                displayName: column, 
+                type: null
+            };
+            this.columns.set(columnId, columnObj);
+            return;
+        }
+        this.columns.set(columnId,column)
+    }
     
+    removeColumn(columnId: ColumnID): void {
+        this.columns.delete(columnId);
+        if (this.columnConstraints) {
+            this.columnConstraints.delete(columnId);
+        }
+    }
     
+    addRow(row: RowData): void {
+        const rowID: RowID = this.graveyard.length > 0
+            ? this.graveyard.pop()!
+            : this._rowCounter++
+            ;
+        this.rows.set(rowID, row);
+    }
+
+    removeRow(rowID: RowID): void {
+        this.rows.delete(rowID);
+        if (this._errorMap) this._errorMap.delete(rowID);
+        this.graveyard.push(rowID);
+    }
+
+    generateUniqunessMap(): void{
+        if (this._uniquenessMap.size > 0) return;
+        else {
+            
+        }
+    }
 }
